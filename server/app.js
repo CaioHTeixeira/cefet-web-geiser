a618f3b4db6bfc08ed27fd4526380d819ec9d014
CaioHTeixeira <caioteixeiraaa@gmail.com>
2022-01-25 22:10:18 -0300

    exercicio 1

--- server/app.js
+++ server/app.js
@@ -1,9 +1,10 @@
 // importação de dependência(s)
-
+import express from "express"
 
 // variáveis globais deste módulo
 const PORT = 3000
 const db = {}
+const app = express()
 
 
 // carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
@@ -38,6 +39,10 @@ const db = {}
 // configurar para servir os arquivos estáticos da pasta "client"
 // dica: 1 linha de código
 
+app.use(express.static('client'))
 
 // abrir servidor na porta 3000 (constante PORT)
 // dica: 1-3 linhas de código
+app.listen(PORT, () => {
+    console.log(`Estou ouvindo na porta ${PORT}`);
+  })
